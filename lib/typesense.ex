d4d2e1425246dbaf1491b00e92081526f36fb234
user <user@example.com>
2020-12-10 04:53:59 +0100

    first commit

---
+++ lib/typesense.ex
@@ -0,0 +1,18 @@
+defmodule Typesense do
+  @moduledoc """
+  Documentation for `Typesense`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Typesense.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/typesense_test.exs
@@ -0,0 +1,8 @@
+defmodule TypesenseTest do
+  use ExUnit.Case
+  doctest Typesense
+
+  test "greets the world" do
+    assert Typesense.hello() == :world
+  end
+end
