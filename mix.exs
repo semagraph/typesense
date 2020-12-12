88edc981492313c151faa55948a5acc52118f591
semagraph <semagraph@protonmail.com>
2020-12-12 02:15:15 +0100

    changed release to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Typesense.MixProject do
   def project do
     [
       app: :typesense,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       description: "TypeSense API library for Elixir",
